299b32d03ea43288d8a5058df28d1b18b3bad9c2
d-park7 <71139667+d-park7@users.noreply.github.com>
2021-01-03 22:20:42 -0600

    Update testInput.py

--- testInput.py
+++ testInput.py
@@ -1,26 +1,68 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import create_engine
-import pandas as pd
 import sqlite3
+import argparse
+import sys
+import os
 
 
-def add_data():
-    """Add test data"""
-
-    engine = create_engine(f"sqlite:///test.db")
-    conn = engine.connect()
-    df = pd.DataFrame(
-        [(1, "John", "Doe")], columns=["EmployeeId", "FirstName", "LastName"]
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n",
+        "--name",
+        type=str,
+        required=True,
+        help="Name of database"
     )
-    df.to_sql("Employee", con=engine, if_exists="append")
+    args = parser.parse_args()
+    return args
+
+
+def input_data(args):
+    """
+    Inputs test data into the database
+    :param db_name:
+    :return sqlite3 connection:
+    """
+    db_name = add_filename_extension(args.name)
+    dbExists = True
+    # try to connect to the named db
+    # if it does not exist quit the program
+    try:
+        conn = sqlite3.connect(db_name)
+    except Exception:
+        dbExists = False
+        pass
+    if dbExists == False:   
+        print("Error: Database does not exist", file=sys.stderr)
+        quit()
+
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Employee VALUES (2 , 'asdf', 'Dsd')")
+    cursor.execute("INSERT INTO Record VALUES (2, '2020-01-01 08:00:00', '2020-01-01 17:00:00')")
+    cursor.execute("INSERT INTO Pay VALUES (2, 10, '2020-01-01', '2020-01-15')")
     conn.commit()
     conn.close()
 
 
+def add_filename_extension(db_name: str):
+    """
+    Adds the .db ext if the name does not contain it
+    :param db_name:
+    :return:
+    """
+    filename, file_extension = os.path.splitext(db_name)
+    if file_extension:
+        return db_name
+    else:
+        return filename + ".db"
+
+
 def main():
-    """Driver function"""
-    add_data()
+    args = parse_args()
+    input_data(args)
 
 
 if __name__ == "__main__":
