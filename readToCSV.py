75af76a568e65e0642011f23e39c9d1851843bdd
d-park7 <71139667+d-park7@users.noreply.github.com>
2021-01-19 15:00:11 -0600

    Add files via upload

--- readToCSV.py
+++ readToCSV.py
@@ -10,23 +10,24 @@ def pargse_args():
     # argument parser
     parser = argparse.ArgumentParser(description="Read from the inputted database")
     parser.add_argument(
-        "-n",
-        "--name",
+        "-d",
+        "--dbname",
         type=str,
         required=True,
         help="Name of db file")
     parser.add_argument(
-        "-r",
-        "--read",
-        action="store_true",
-        help="Read the data into a csv"
+        "-c",
+        "--csvname",
+        type=str,
+        required=True,
+        help="Name of csv file"
     )
 
     args = parser.parse_args()
     return args
 
 
-def read(args):
+def read_to_csv(args):
     """
     Reads from db to a csv file
     :param db_name:
@@ -34,7 +35,8 @@ def read(args):
     """
     # try to connect to the named db
     # if it does not eist quit the program
-    db_name = add_filename_extension(args.name)
+    db_name = add_filename_extension(args.dbname, "db")
+    csv_name = add_filename_extension(args.csvname, "csv")
     dbExists = True
     try:
         conn = sqlite3.connect(db_name)
@@ -45,38 +47,42 @@ def read(args):
         print("Error: Datababse does not exist", file=sys.stderr)
         os.system("PAUSE")
         quit()
-    db_name = add_filename_extension(args.name)
-    if args.read:
-        conn = sqlite3.connect(db_name)
 
-        # query database for data
-        SQLQuery = 'SELECT * FROM Record'
-
-        # create a dataframe from database using pandas
-        df = pd.read_sql_query(SQLQuery, conn)
-        df.to_csv('testInfo.csv')
-        print(df)
+    conn = sqlite3.connect(db_name)
+    # query database for data
+    SQLQuery = 'SELECT * FROM Record;'
+    # create a dataframe from database using pandas
+    df = pd.read_sql_query(SQLQuery, conn)
+    df.to_csv(csv_name)
 
     conn.close()
 
 
-def add_filename_extension(db_name: str):
+def add_filename_extension(datafile: str, file_type: str):
     """
-    Adds the .db ext if the name does not contain it
-    :param db_name:
-    :return:
+    Adds the .db or .csv ext if the name does not contain it
+    :param args:
+    :return full_file_name:
     """
-    filename, file_extension = os.path.splitext(db_name)
-    if file_extension:
-        return db_name
-    else:
-        return filename + ".db"
+    if file_type == "db":
+        filename, file_extension = os.path.splitext(datafile)
+        if file_extension:
+            full_file_name = datafile
+        else:
+            full_file_name = args.dbname + ".db"
+    elif file_type == "csv":
+        filename, file_extension = os.path.splitext(datafile)
+        if file_extension:
+            full_file_name = datafile
+        else:
+            full_file_name = args.csvname + ".csv"
+            
+    return full_file_name
 
 
 def main():
     args = pargse_args()
-    read(args)
-    os.system("PAUSE")
+    read_to_csv(args)
 
 
 if __name__ == "__main__":
