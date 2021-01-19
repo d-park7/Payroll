5406b5a9e5b22b0f4f6bb79c8652ec1cd0b2ec16
d-park7 <71139667+d-park7@users.noreply.github.com>
2021-01-19 15:00:54 -0600

    Add files via upload

--- calcPay.py
+++ calcPay.py
@@ -18,9 +18,14 @@ def parse_args():
         help="Name of db file")
     parser.add_argument(
         "-c",
+        "--calculate",
+        action="store_true",
+        help="Calculate the pay based on the time in and time out"
+    )
+    parser.add_argument(
+        "-v",
         "--csvname",
         type=str,
-        required=True,
         help="Name of csv file to read"
     )
 
@@ -31,12 +36,36 @@ def parse_args():
 def calculate_pay(args):
     """
     Calculates data from the database
-    :param db_name:
+    :param args:
     :return sqlite3 connection:
+    """
+    db_name = add_filename_extension(args.dbname, "db")
+
+    if args.csvname:
+        csv_name = add_filename_extension(args.csvname, "csv")
+        df = read_csv_name(db_name, csv_name)
+    else:
+        conn = sqlite3.connect(db_name)
+        SQLQuery = 'SELECT * FROM Record'
+        df = pd.read_sql_query(SQLQuery, conn)
+
+    if args.calculate:
+        df['DateTimeIn'] = pd.to_datetime(df['DateTimeIn'])
+        df.DateTimeOut = pd.to_datetime(df.DateTimeOut)
+        print(df.loc[0, 'DateTimeIn'].day_name())
+        print(df.loc[0, "DateTimeOut"].day_name())
+        print(df["DateTimeIn"].dt.day_name())
+
+
+
+def read_csv_name(db_name: str, csv_name: str):
+    """
+    Reads the csv file and converts it into a dataframe format
+    :param csv_name:
+    :return df:
     """
     # try to connect to the named db
     # if it does not eist quit the program
-    db_name = add_filename_extension(args.dbname)
     dbExists = True
     try:
         conn = sqlite3.connect(db_name)
@@ -48,33 +77,33 @@ def calculate_pay(args):
         os.system("PAUSE")
         quit()
 
-    if args.csvname:
-        # accept a csv file name and then put the csv into a dataframe using pandas
-        csv_name = args.csvname
-        conn = sqlite3.connect(db_name)
-        df = pd.read_csv(csv_name)
-        # isolate the DateTimeIn/DateTimeOut columns from the df and change the objects into a datetime variable type
-        df['DateTimeIn'] = pd.to_datetime(df.DateTimeIn)
-        df['DateTimeOut'] = pd.to_datetime(df.DateTimeOut)
-        print(df)
-        print(df.dtypes)
-        print("time in hours: ", df.DateTimeIn.dt.hour)
-        print("time out hours: ", df.DateTimeOut.dt.hour)
-        
+    # accept a csv file name and then put the csv into a dataframe using pandas
+    conn = sqlite3.connect(db_name)
+    df = pd.read_csv(csv_name)
     conn.close()
+    return df
 
 
-def add_filename_extension(db_name: str):
+def add_filename_extension(datafile: str, file_type: str):
     """
-    Adds the .db ext if the name does not contain it
-    :param db_name:
-    :return:
+    Adds the .db or .csv ext if the name does not contain it
+    :param args:
+    :return full_file_name:
     """
-    filename, file_extension = os.path.splitext(db_name)
-    if file_extension:
-        return db_name
-    else:
-        return filename + ".db"
+    if file_type == "db":
+        filename, file_extension = os.path.splitext(datafile)
+        if file_extension:
+            full_file_name = datafile
+        else:
+            full_file_name = args.dbname + ".db"
+    elif file_type == "csv":
+        filename, file_extension = os.path.splitext(datafile)
+        if file_extension:
+            full_file_name = datafile
+        else:
+            full_file_name = args.csvname + ".csv"
+            
+    return full_file_name
 
 
 def main():
