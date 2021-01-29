8ce162592bba63bed1075152547368824b0a2505
d-park7 <71139667+d-park7@users.noreply.github.com>
2021-01-28 22:43:48 -0600

    Changed database schema and added keys
    Changed the database schema and added primary and foreign keys

--- initdb.py
+++ initdb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from initdb import add_filename_extension
 import sqlite3
 import argparse
 import sys
@@ -7,80 +8,32 @@ import os
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Initialize the db")
+    parser = argparse.ArgumentParser()
     parser.add_argument(
         "-n",
         "--name",
         type=str,
         required=True,
-        help="Name of db file"
+        help="Name of database"
     )
-    parser.add_argument(
-        "-c", "--create", action="store_true", help="Create the db if not existing"
-    )
-    parser.add_argument(
-        "-o", "--override", action="store_true", help="Override the specified db"
-    )
-
     args = parser.parse_args()
     return args
 
 
-def init_db(args):
-    db_name = add_filename_extension(args.name)
-
-    if args.create:
-        conn = create_new_db(db_name)
-    if args.override:
-        delete_existing_db(db_name)
-        conn = create_new_db(db_name)
-
-    if conn.total_changes != 0:
-        print("Failed to create database.", file=sys.stderr)
-        quit()
-
-    conn.commit()
-    conn.close()
-    return
-
-
-def create_new_db(db_name: str):
+def input_data(args):
     """
-    Creates a new database if not already made
+    Inputs test data into the database
     :param db_name:
     :return sqlite3 connection:
     """
-    test_database_connection(db_name)
+    db_name = add_filename_extension(args.name)
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
-    create_tables(cursor)
-    return conn
-
-
-def delete_existing_db(db_name: str):
-    """
-    Deletes existing db and replaces it
-    :param db_name:
-    :return sqlite3 connection:
-    """
-    # try to delete the db file
-    if os.path.exists(db_name):
-            os.remove(db_name)
-    else:
-        print("Error: Database does not exist", file=sys.stderr)
-        quit()
-
-
-def create_tables(cursor):
-    cursor.execute(
-        "CREATE TABLE Employee (EmployeeId int, FirstName text, LastName text)"
-    )
-    cursor.execute(
-         "CREATE TABLE Record (RecordId int, DateTimeIn text, DateTimeOut text, DailyWage unsigned float)"
-    )
-    cursor.execute(
-        "CREATE TABLE Pay (PayId int, PayPerHour unsigned float, BiWeeklyStartDate text, BiWeeklyEndDate text)"
-    )
+    cursor.execute("INSERT INTO Employee VALUES (1, 'John', 'Doe')")
+    cursor.execute("INSERT INTO Record VALUES (1, '2021-01-01', '09:00:00', '17:00:00')")
+    cursor.execute("INSERT INTO Pay VALUES (1, '2021-01-01', '10')")
+    conn.commit()
+    conn.close()
 
 
 def test_database_connection(db_name: str):
@@ -91,12 +44,12 @@ def test_database_connection(db_name: str):
     """
     dbExists = True
     try:
-        conn = sqlite3.connect("file:{}?mode=rw".format(db_name), uri=True)
+        conn = sqlite3.connect(db_name)
     except Exception as e:
         dbExists = False
         pass
-    if dbExists == True:
-        print("Error: Database already exists", file=sys.stderr)
+    if dbExists == False:   
+        print("Error: Database does not exist", file=sys.stderr)
         quit()
 
 
@@ -115,7 +68,7 @@ def add_filename_extension(db_name: str):
 
 def main():
     args = parse_args()
-    init_db(args)
+    input_data(args)
 
 
 if __name__ == "__main__":
