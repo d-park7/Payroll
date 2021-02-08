47f44e99c2ba6879925020dc1ffa1a3f4ba5f475
Mark <2017mark@gmail.com>
2021-02-07 23:05:24 -0600

    Setup test POST/GET requests in Flask App

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, jsonify, redirect, url_for, render_template
+from flask import Flask, jsonify, redirect, url_for, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def home():
-    return render_template('home.html')
 
-# @app.route('/test')
-# def test():
-#     return redirect(url_for("home", name="admin!", content=['apple', 'orange']))
+    # If searching for employee, fetch data, otherwise load page
+    if request.method == "POST":
+        employee_id = request.form['employee_id']
+        return redirect('test')
+    else:
+        return render_template('home.html')
+
+
+@app.route('/test')
+def test():
+    return f'<h1>Test page here</h1>'
 
 # Run flask app on localhost
 if __name__ == '__main__':
