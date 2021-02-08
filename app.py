daccfd2594d4b9212c8c22a05e03220f4992cbc4
Mark <2017mark@gmail.com>
2021-02-07 22:15:34 -0600

    Adding base html template to project

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Click here</h1>'
+def home():
+    return render_template('home.html')
+
+# @app.route('/test')
+# def test():
+#     return redirect(url_for("home", name="admin!", content=['apple', 'orange']))
 
 # Run flask app on localhost
 if __name__ == '__main__':
