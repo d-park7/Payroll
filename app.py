07202423ab751fdcc1ad8acb48b211af890a1e9b
Mark <2017mark@gmail.com>
2021-02-14 17:18:06 -0600

    Setting up html pages

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
-from flask import Flask, jsonify, redirect, url_for, render_template, request
+from flask import Flask, jsonify, redirect, url_for, render_template, request, session
+from datetime import timedelta
+
 app = Flask(__name__)
+app.secret_key = "PAYROLL"
+app.permanent_session_lifetime = timedelta(minutes=30)
 
-@app.route('/', methods=["POST", "GET"])
+session = dict()
+
+@app.route('/home', methods=["POST", "GET"])
 def home():
-
     # If searching for employee, fetch data, otherwise load page
     if request.method == "POST":
-        employee_id = request.form['employee_id']
-        return redirect('test')
+        return f'<h1>Fetch employee id</h1>'
     else:
         return render_template('home.html')
 
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    # If searching for employee, fetch data, otherwise load page
+    if request.method == "POST":
+        user = request.form["eid"]
+        session['user'] = user
+        return redirect(url_for("home"))
+    else:
+        return render_template('login.html')
 
-@app.route('/test')
-def test():
-    return f'<h1>Test page here</h1>'
+# When user logs out, clear session data and redirect to login page
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for("login"))
 
 # Run flask app on localhost
 if __name__ == '__main__':
