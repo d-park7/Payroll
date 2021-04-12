dcb339b92b502b5887469dd940d4e3fc4ac65768
d-park7 <71139667+d-park7@users.noreply.github.com>
2021-04-12 16:57:11 -0500

    Currently learning javascript

--- website/static/index.js
+++ website/static/index.js
@@ -1,15 +1,17 @@
-const button = document.getElementById('clockIn');
+// Currently learning javascript
 
-button.addEventListener('click', async _ => {
-  try {     
-    const response = await fetch('user.html', {
-      method: 'post',
-      body: {
-        // Your body
-      }
-    });
-    console.log('Completed!', response);
-  } catch(err) {
-    console.error(`Error: ${err}`);
-  }
-});
+// const button = document.getElementById('clockIn');
+
+// button.addEventListener('click', async _ => {
+//   try {     
+//     const response = await fetch('user.html', {
+//       method: 'post',
+//       body: {
+// 
+//       }
+//     });
+//     console.log('Completed!', response);
+//   } catch(err) {
+//     console.error(`Error: ${err}`);
+//   }
+// });
